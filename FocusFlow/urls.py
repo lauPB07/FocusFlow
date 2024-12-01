edf301ff954b766725735d38c23d19fe2636667f
lauPB07 <laurapiresberclaz@gmail.com>
2024-12-01 22:50:00 +0100

    feat : creation de la connection et deconnection de notre webapp

--- FocusFlow/urls.py
+++ FocusFlow/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from FocusFlow import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('acceuil/', views.acceuil_views, name='acceuil'),
+    path('logout/', views.logout_views, name='logout')
 ]
--- FocusFlow/views.py
+++ FocusFlow/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+
+        if user is not None:
+            login(request, user)
+            print(user)
+            return redirect('acceuil')
+        else:
+            messages.info(request, 'Identifiant ou mot de passe incorrect')
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def acceuil_views(request):
     return render(request, 'acceuil.html')
+
+def logout_views(request):
+    logout(request)
+    return redirect('index')
