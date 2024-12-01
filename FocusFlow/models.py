62f518c08e71445bf1b96a162905758959e45388
lauPB07 <laurapiresberclaz@gmail.com>
2024-12-01 21:24:14 +0100

    feat : ajout de la structure de la base de donée + creation de la page base.html pour la page principale de notrewebapp avec le css et html voulue

--- FocusFlow/models.py
+++ FocusFlow/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Projet(models.Model):
+    nom = models.CharField(max_length=255)
+    description = models.TextField()
+    createdBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User, related_name='participes')
+
+class Status(models.Model):
+    nom = models.CharField(max_length=255)
+
+
+class Tache(models.Model):
+    nom = models.CharField(max_length=255)
+    description = models.TextField()
+    createdBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name='createdTasks')
+    realizedBy = models.ForeignKey(User, on_delete=models.CASCADE, related_name='realizedTasks')
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    projet = models.ManyToManyField(Projet, related_name='projetsTaches')
+
+
